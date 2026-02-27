4ea9666a3301b6162a99a45b692b97c36a40bcab
acculief <acculief@gmail.com>
2026-02-28 03:09:57 +0900

    fix: add prisma generate to build, remove invalid config files

--- prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import path from 'node:path'
-import type { PrismaConfig } from 'prisma'
-
-export default {
-  earlyAccess: false,
-  schema: path.join('prisma', 'schema.prisma'),
-} satisfies PrismaConfig
